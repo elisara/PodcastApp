c96762d6b92ed3bbfa4524870854537169ce50c2
Elisa Rajaniemi <elisa.rajaniemi@gmail.com>
2016-10-26 12:23:04 +0300

    tool bar icons, menuFragment, category button and spinner

--- app/src/main/java/com/example/elisarajaniemi/podcastapp/MainActivity.java
+++ app/src/main/java/com/example/elisarajaniemi/podcastapp/MainActivity.java
@@ -1,23 +1,68 @@
 package com.example.elisarajaniemi.podcastapp;
 
+import android.app.ActionBar;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ImageButton;
+import android.widget.Spinner;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener{
+
+    private ImageButton menuBtn;
+    private Spinner spinner;
+
+    private MenuFragment mf;
+    private TextView title;
+
+    private boolean menuOpen;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+        //setSupportActionBar(toolbar);
+        title = (TextView) toolbar.findViewById(R.id.title);
+        spinner = (Spinner) findViewById(R.id.spinner);
+        spinner.setOnItemSelectedListener(this);
+        addItemsOnSpinner();
 
+        menuOpen = false;
+
+        menuBtn = (ImageButton) findViewById(R.id.menuBtn);
+        menuBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                if(menuOpen == false) {
+                    getSupportFragmentManager().beginTransaction()
+                            .replace(R.id.frag_container, mf).commit();
+                    menuOpen = true;
+                }
+                else{
+                    getSupportFragmentManager().beginTransaction()
+                            .remove(mf).commit();
+                    menuOpen = false;
+                }
+
+                System.out.println("menu clicked");
+
+            }
+        });
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -49,4 +94,29 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+    public void addItemsOnSpinner() {
+        List<String> list = new ArrayList<String>();
+        list.add("NAME");
+        list.add("NEW");
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, list);
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(dataAdapter);
+        System.out.println("added items to spinner");
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+        String value = parent.getItemAtPosition(position).toString();
+        if(value.contains("NAME")){
+            System.out.println("SORT: NAME");
+        }
+        else if(value.contains("NEW")){
+            System.out.println("SORT: NEW");
+        }
+    }
+    public void onNothingSelected(AdapterView<?> arg0) {
+        // TODO Auto-generated method stub
+    }
+
 }
---
+++ app/src/main/java/com/example/elisarajaniemi/podcastapp/MenuFragment.java
@@ -0,0 +1,36 @@
+package com.example.elisarajaniemi.podcastapp;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+
+import static android.content.Context.MODE_APPEND;
+
+/**
+ * Created by Elisa Rajaniemi on 26.10.2016.
+ */
+
+public class MenuFragment extends Fragment {
+
+    private Button close;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.menu_layout, container, false);
+
+        return view;
+    }
+}
+
+
