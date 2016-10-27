8c6516cfce58013ec5787c0015074b3945445226
Keni <keni.kastinen@metropolia.fi>
2016-10-27 15:40:22 +0300

    merge testi

--- app/src/main/java/com/example/elisarajaniemi/podcastapp/MainActivity.java
+++ app/src/main/java/com/example/elisarajaniemi/podcastapp/MainActivity.java
@@ -33,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        //kommentti
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
