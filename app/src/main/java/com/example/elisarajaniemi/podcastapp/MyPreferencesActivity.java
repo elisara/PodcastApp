e2821d47de605e197e46b3e3625f79eb2b0e4b4e
Elisa Rajaniemi <elisa.rajaniemi@gmail.com>
2016-10-28 12:40:28 +0300

    sharedpreferenceschangedlistener

--- app/src/main/java/com/example/elisarajaniemi/podcastapp/MyPreferencesActivity.java
+++ app/src/main/java/com/example/elisarajaniemi/podcastapp/MyPreferencesActivity.java
@@ -1,11 +1,17 @@
 package com.example.elisarajaniemi.podcastapp;
 
+import android.app.Activity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.CheckBoxPreference;
+import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceFragment;
 import android.preference.PreferenceManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 
 /**
  * Created by Elisa Rajaniemi on 28.10.2016.
@@ -13,14 +19,33 @@ import android.widget.Button;
 
 public class MyPreferencesActivity extends PreferenceActivity {
 
+    boolean checked;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.preference_layout);
 
-        getFragmentManager().beginTransaction()
-                .replace(R.id.displayPrefs, new MyPreferenceFragment()).commit();
+        getFragmentManager().beginTransaction().replace(R.id.displayPrefs, new MyPreferenceFragment()).commit();
         PreferenceManager.setDefaultValues(this, R.xml.preferences, false);
+        SharedPreferences sp1 = PreferenceManager.getDefaultSharedPreferences(this);
+        SharedPreferences sp = getSharedPreferences("myCustomSharedPrefs", Activity.MODE_PRIVATE);
+        checked = sp1.getBoolean("history", false);
+        //String strUsr = sp1.getString("history", "NA");
+        System.out.println(checked);
+
+        SharedPreferences.OnSharedPreferenceChangeListener listener = new SharedPreferences.OnSharedPreferenceChangeListener() {
+            public void onSharedPreferenceChanged(SharedPreferences prefs, String key) {
+                if(key.equals("history")){
+                    System.out.println("PREF_KEY: " + key);
+                }
+
+                System.out.println(checked);
+            }
+        };
+
+        sp.registerOnSharedPreferenceChangeListener(listener);
+
 
         Button button = (Button) findViewById(R.id.close);
         button.setOnClickListener(new View.OnClickListener() {
@@ -28,8 +53,8 @@ public class MyPreferencesActivity extends PreferenceActivity {
                 finish();
             }
         });
-
     }
+
     public static class MyPreferenceFragment extends PreferenceFragment  {
         @Override
         public void onCreate(final Bundle savedInstanceState) {
