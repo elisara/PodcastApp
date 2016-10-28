27b8a1594aab4f50eb829ff953555f8a3aca7194
Keni <keni.kastinen@metropolia.fi>
2016-10-28 10:50:24 +0300

    Moving to PlayerFragment working

--- app/src/main/java/com/example/elisarajaniemi/podcastapp/MainActivity.java
+++ app/src/main/java/com/example/elisarajaniemi/podcastapp/MainActivity.java
@@ -35,8 +35,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
-        //kommentti
-        //kommentti 2
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
