1a130807623257d64e030ff360020fc2578f3865
Elisa Rajaniemi <elisa.rajaniemi@gmail.com>
2016-10-31 10:56:52 +0200

    menu UI changes

--- app/src/main/java/com/example/elisarajaniemi/podcastapp/MenuFragment.java
+++ app/src/main/java/com/example/elisarajaniemi/podcastapp/MenuFragment.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.FrameLayout;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -23,37 +24,17 @@ public class MenuFragment extends Fragment {
     private SerieArrayAdapter adapter;
     private Button closeMenu;
     private MainActivity ma;
+    private FrameLayout fl;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        final ArrayList<String> list = new ArrayList<>();
-        list.add("item1");
-        list.add("item2");
-        list.add("item3");
-        list.add("item4");
-        list.add("item5");
 
         View view = inflater.inflate(R.layout.menu_layout, container, false);
 
-        ma = new MainActivity();
-
-        listView = (ListView) view.findViewById(R.id.menuListView);
-        adapter = new SerieArrayAdapter(getContext(),list);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> av, View v, int position, long rowId) {
-                String value = list.get(position);
-                System.out.println(value);
-            }
-
-        });
-
-        closeMenu = (Button) view.findViewById(R.id.closeMenu);
-        closeMenu.setOnClickListener(new View.OnClickListener() {
+        fl = (FrameLayout) view.findViewById(R.id.outside);
+        fl.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-
+                System.out.println("outside");
             }
         });
 
